97513164110938d8bc4299edf78b4536603408dd
poxwed <poxwedfx@gmail.com>
2024-04-23 20:41:41 +0200

    aaaa

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ let username;
 
 document.getElementById("submit").onclick = function(){
     username = document.getElementById("text").value;
-    document.getElementById("H1").textContent = "Now I have your IP lil bro NayutaS"
+    document.getElementById("H1").textContent = "Now I have your IP lil bro Nayuta"
 }
