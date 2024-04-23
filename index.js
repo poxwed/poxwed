0e582c3ed39a5c4d2f23ad8e51b1292c0e1151db
poxwed <poxwedfx@gmail.com>
2024-04-23 20:39:14 +0200

    assigment

---
+++ index.js
@@ -0,0 +1,7 @@
+let username;
+
+
+document.getElementById("submit").onclick = function(){
+    username = document.getElementById("text").value;
+    document.getElementById("H1").textContent = "Now I have your IP lil bro NayutaS"
+}
